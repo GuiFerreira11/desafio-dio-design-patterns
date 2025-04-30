54822c4f687439ed97a46e311cf817e33f7aa684
Gui Ferreira <guifl_pereira@hotmail.com>
2025-04-30 12:06:44 -0300

    Primeiro commit

---
+++ java/src/main/java/desafio/dio/design_patterns/java/Main.java
@@ -0,0 +1,7 @@
+package desafio.dio.design_patterns.java;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ java/src/test/java/desafio/dio/design_patterns/java/AppTest.java
@@ -0,0 +1,19 @@
+package desafio.dio.design_patterns.java;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
